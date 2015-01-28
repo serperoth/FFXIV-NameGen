bdc8930d972c3eb85b7dc49f8375859942de640b
serperoth <fallen.paladin@hotmail.com>
2015-01-29 01:02:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include "namelists.h"
 
 int main(void){
-  
+  int race, subrace;
+  //Message to ask the user to input race, as a number from 1-5.
+  cin << race;
+  //Needs to make sure an int is indeed inputted
+  switch(race)
   return 0;
 }
